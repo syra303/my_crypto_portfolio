b23540faa487a8f1852f710506738bda5dbef526
Emmanuel L Constant <constantemmanuel78@gmail.com>
2021-08-28 10:07:47 -0400

    Updated Coinbase to include buy and sell data pull functions and order data

--- Coinbase/CoinbaseData.py
+++ Coinbase/CoinbaseData.py
@@ -75,6 +75,8 @@ def get_address_list(client, accounts_ids):
              if addressData['resource'] == 'address':
                  addresses.append({'account_id': id,'address_id': str(addressData['id']), 'address': str(addressData['address']), 
                  'coin_network': str(addressData['network']), 'update_date': str(addressData['updated_at'])})
+             else:
+                print(f'No addresses listed for account id: {id}')
                  
      #store list of address info into a dataframe             
     df_address_list = pd.DataFrame(addresses)
@@ -122,6 +124,104 @@ def get_transaction_list(client, address_ids):
 
 
 
+def get_buy_list(client, accounts_ids):
+    """
+    Function to pull all buy orders per coinbase account id
+    The function takes two arguments as parameters: 
+
+        - client: client coinbase object from the api_connect function 
+        - accounts_ids: list containing account ids
+
+    The function returns a dataframes with buy order details
+    """
+
+    #list to store addresses
+    buy_orders = []
+
+    #pull address info from address data
+    for id in accounts_ids:
+         buyData = client.get_buys(id)
+         for buys in buyData.data:
+             if buys['resource'] == 'buy':
+                 buy_orders.append({'account_id': id,'buy_order_id': str(buys['id']), 'buy_status': str(buys['status']), 
+                 'payment_method_id': str(buys['payment_method']['id']),'payment_method_resource': str(buys['payment_method']['resource']),
+                 'tx_id': str(buys['transaction']['id']),'tx_resource': str(buys['transaction']['resource']),'buy_amount': str(buys['amount']['amount']),
+                 'buy_amount_currency': str(buys['amount']['currency']),'buy_total': str(buys['total']['amount']),'buy_total_currency': str(buys['total']['currency']),
+                 'buy_sub_total': str(buys['subtotal']['amount']),'buy_sub_total_currency': str(buys['subtotal']['currency']),
+                 'buy_date': str(buys['created_at']),'buy_update_date': str(buys['updated_at']),'resource': str(buys['resource']),'buy_committed': str(buys['committed']),
+                 'buy_instant': str(buys['instant']),
+                 #'buy_fee_amount': str(buys['fee']['amount']),
+                 #'buy_fee_currency': str(buys['fee']['currency']),
+                 'buy_payout_date': str(buys['payout_at'])})
+             else:
+                print(f'No buy data for account id: {id}')
+
+    #check for buy order data
+    if bool(buy_orders) == True:
+        #store list of address info into a dataframe             
+        df_buy_orders = pd.DataFrame(buy_orders)
+
+        #convert date strings to dates format
+        df_buy_orders['buy_date'] = pd.to_datetime(df_buy_orders['buy_date'],format='%Y-%m-%d')
+        df_buy_orders['buy_update_date'] = pd.to_datetime(df_buy_orders['buy_update_date'],format='%Y-%m-%d')
+        df_buy_orders['buy_payout_date'] = pd.to_datetime(df_buy_orders['buy_payout_date'],format='%Y-%m-%d')    
+
+        return df_buy_orders
+    else:
+        print('No buy orders to pull at this time')
+    
 
 
+def get_sell_list(client, accounts_ids):
+    """
+    Function to pull all sell orders per coinbase account id
+    The function takes two arguments as parameters: 
+
+        - client: client coinbase object from the api_connect function 
+        - accounts_ids: list containing account ids
+
+    The function returns a dataframes with sell order details
+    """
+
+    #list to store addresses
+    sell_orders = []
+    
+    #pull address info from address data
+    for id in accounts_ids:
+        sellData = client.get_sells(id)
+        for sells in sellData.data:
+             if sells['resource'] == 'sell':
+             #print(sells)
+                 sell_orders.append({'account_id': id,'sell_order_id': str(sells['id']), 'sell_status': str(sells['status']), 
+                 'payment_method_id': str(sells['payment_method']['id']),'payment_method_resource': str(sells['payment_method']['resource']),
+                 'tx_id': str(sells['transaction']['id']),'tx_resource': str(sells['transaction']['resource']),'sell_amount': str(sells['amount']['amount']),
+                 'sell_amount_currency': str(sells['amount']['currency']),'sell_total': str(sells['total']['amount']),'sell_total_currency': str(sells['total']['currency']),
+                 'sell_sub_total': str(sells['subtotal']['amount']),'sell_sub_total_currency': str(sells['subtotal']['currency']),
+                 #'sell_date': str(sells['created_at']),
+                 'sell_update_date': str(sells['updated_at']),
+                 'resource': str(sells['resource']),
+                 'sell_committed': str(sells['committed']),
+                 'sell_instant': str(sells['instant']),
+                 'sell_fee_amount': str(sells['fee']['amount']),
+                 'sell_fee_currency': str(sells['fee']['currency']),
+                 'sell_payout_date': str(sells['payout_at'])})
+             else:
+                print(f'No sell data for account id: {id}')
+
+    #check for sell order data           
+    if bool(sell_orders) == True:
+
+         #store list of address info into a dataframe
+         df_sell_orders = pd.DataFrame(sell_orders)
+
+         #convert date strings to dates format
+         df_sell_orders['sell_date'] = pd.to_datetime(df_sell_orders['sell_date'],format='%Y-%m-%d')
+         df_sell_orders['sell_update_date'] = pd.to_datetime(df_sell_orders['sell_update_date'],format='%Y-%m-%d')
+         df_sell_orders['sell_payout_date'] = pd.to_datetime(df_sell_orders['sell_payout_date'],format='%Y-%m-%d')
+
+         return df_sell_orders
+    else:
+        print('No sell orders to pull at this time')
+
+    
 
